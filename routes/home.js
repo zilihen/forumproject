17325a0ea505cfbd9e58348dda1cdeddd3798ddf
Jason Chen <jasonchen7766@gmail.com>
2026-05-09 11:55:50 -0400

    fixed stuff hopefully?

--- routes/home.js
+++ routes/home.js
@@ -17,47 +17,43 @@ const Joke = mongoose.model("Joke", jokeSchema);
 let jokeSetup = "";
 let jokePunchline = "";
 
-// generateJoke();
+generateJoke();
 
 router.get("/", (req, res) => {
   res.render("home", { name: cred.getName() });
 });
 
-// async function generateJoke() {
-//   console.log("pressing the button did something?");
-//   const response = await fetch(
-//     "https://official-joke-api.appspot.com/jokes/random",
-//     {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     },
-//   );
-//   const data = await response.json();
-//   jokeSetup = data.setup;
-//   jokePunchline = data.punchline;
-//   text = `${setup}\n${punchline}`;
-//   document.querySelector("#textZone").value = text;
-// }
+async function generateJoke() {
+  console.log("pressing the button did something?");
+  const response = await fetch(
+    "https://official-joke-api.appspot.com/jokes/random",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const data = await response.json();
+  jokeSetup = data.setup;
+  jokePunchline = data.punchline;
+  text = `${setup}\n${punchline}`;
+  document.querySelector("#textZone").value = text;
+}
 
-// async function saveJoke() {
-//   try {
-//     await mongoose.connect(uri);
-//     const jokeInfo = new Joke({
-//       username: user,
-//       setup: jokeSetup,
-//       punchline: jokePunchline,
-//     });
-//     await jokeInfo.save();
-//     mongoose.disconnect();
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-
-function saveJoke() { 
-  console.log("hello")
+async function saveJoke() {
+  try {
+    await mongoose.connect(uri);
+    const jokeInfo = new Joke({
+      username: user,
+      setup: jokeSetup,
+      punchline: jokePunchline,
+    });
+    await jokeInfo.save();
+    mongoose.disconnect();
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 
