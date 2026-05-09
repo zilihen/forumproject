89176f7e8727ca471f4473cac3fbb239b0d07727
Jason Chen <jasonchen7766@gmail.com>
2026-05-09 12:33:16 -0400

    added some TOOD

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,30 @@
-function saveJoke() { 
-  console.log("hello");
+async function generateJoke() {
+
+  // send a request to the url https://.../home/generate
+  try {
+    let res = await fetch("/home/generate", {
+      method: "Post"
+    });
+
+    let data = await res.json();
+    let text = `${data.setup}\n${data.punchline}`;
+
+    console.log(text);
+    document.querySelector("#textZone").innerHTML = text;
+  } catch (err) {
+    document.querySelector("#textZone").innerHTML = "Something went wrong.";
+  }
 }
 
+
+// TODO: should be somewhat similar to above. Use response.ok to check for successful response
+async function saveJoke() {
+  try { 
+    let res = await fetch("/home/save")
+  } catch(err) { 
+
+  }
+}
 
 
 
--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,7 @@ const path = require("path");
 const router = express.Router();
 const mongoose = require("mongoose");
 const cred = require("./loginPage");
+const { rmSync } = require("fs");
 
 const jokeSchema = new mongoose.Schema({
   username: String,
@@ -17,44 +18,64 @@ const Joke = mongoose.model("Joke", jokeSchema);
 let jokeSetup = "";
 let jokePunchline = "";
 
-generateJoke();
 
 router.get("/", (req, res) => {
   res.render("home", { name: cred.getName() });
 });
 
-async function generateJoke() {
-  console.log("pressing the button did something?");
-  const response = await fetch(
-    "https://official-joke-api.appspot.com/jokes/random",
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    },
-  );
-  const data = await response.json();
-  jokeSetup = data.setup;
-  jokePunchline = data.punchline;
-  text = `${setup}\n${punchline}`;
-  document.querySelector("#textZone").value = text;
-}
 
-async function saveJoke() {
-  try {
-    await mongoose.connect(uri);
-    const jokeInfo = new Joke({
-      username: user,
-      setup: jokeSetup,
-      punchline: jokePunchline,
-    });
-    await jokeInfo.save();
-    mongoose.disconnect();
-  } catch (err) {
-    console.error(err);
+// async function generateJoke() {
+//   console.log("pressing the button did something?");
+//   const response = await fetch(
+//     "https://official-joke-api.appspot.com/jokes/random",
+//     {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     },
+//   );
+//   const data = await response.json();
+//   jokeSetup = data.setup;
+//   jokePunchline = data.punchline;
+//   text = `${setup}\n${punchline}`;
+//   document.querySelector("#textZone").value = text;
+// }
+
+// this will send a json response back to the function that made the fetch request to https://.../home/generate
+router.post("/generate", async (req, res) => { 
+  let response = await fetch("https://official-joke-api.appspot.com/jokes/random"); 
+  const data = await response.json(); 
+  jokeSetup = data.setup
+  jokePunchline = data.punchline; 
+
+  const joke = { 
+    setup: data.setup, 
+    punchline: data.punchline
   }
-}
+
+  res.json(joke);
+})
+
+
+router.post("/save", async (req, res) => { 
+
+  res.sendStatus(200); // placeholder, sent a OK status back
+})
+// async function saveJoke() {
+//   try {
+//     await mongoose.connect(uri);
+//     const jokeInfo = new Joke({
+//       username: user,
+//       setup: jokeSetup,
+//       punchline: jokePunchline,
+//     });
+//     await jokeInfo.save();
+//     mongoose.disconnect();
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
 
 module.exports = router;
